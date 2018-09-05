82911dab6516b4088330a79618577da3a25b19d9
Kent C. Dodds <kent@doddsfamily.us>
2018-09-04 21:38:37 -0600

    add spacing

--- src/post-editor.js
+++ src/post-editor.js
@@ -35,10 +35,13 @@ class Editor extends Component {
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="title-input">Title</label>
         <input id="title-input" name="title" />
+
         <label htmlFor="content-input">Content</label>
         <textarea id="content-input" name="content" />
+
         <label htmlFor="tags-input">Tags</label>
         <input id="tags-input" name="tags" />
+
         <button type="submit" disabled={this.state.isSaving}>
           Submit
         </button>
