cd9b7fad569c5455af258360af8acd63f771ec29
Kent C. Dodds <kent@doddsfamily.us>
2018-09-07 09:13:17 -0600

    whoops

---
+++ src/greeting-loader-02-mocking.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {loadGreeting} from './api'
+
+class GreetingLoader extends Component {
+  inputRef = React.createRef()
+  state = {greeting: ''}
+  loadGreetingForInput = async () => {
+    const {data} = await loadGreeting(this.inputRef.current.value)
+    this.setState({greeting: data.greeting})
+  }
+  render() {
+    return (
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" ref={this.inputRef} />
+        <button onClick={this.loadGreetingForInput}>Load Greeting</button>
+        <div data-testid="greeting">{this.state.greeting}</div>
+      </div>
+    )
+  }
+}
+
+export {GreetingLoader}
