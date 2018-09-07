3b8599c434312174f1c6986c511a7fb7e8747bcb
Kent C. Dodds <kent@doddsfamily.us>
2018-09-07 09:14:44 -0600

    make it a form

--- src/greeting-loader-01-dependency-injection.js
+++ src/greeting-loader-01-dependency-injection.js
@@ -5,18 +5,19 @@ class GreetingLoader extends Component {
   static defaultProps = {loadGreeting}
   inputRef = React.createRef()
   state = {greeting: ''}
-  loadGreetingForInput = async () => {
+  loadGreetingForInput = async e => {
+    e.preventDefault()
     const {data} = await this.props.loadGreeting(this.inputRef.current.value)
     this.setState({greeting: data.greeting})
   }
   render() {
     return (
-      <div>
+      <form onSubmit={this.loadGreetingForInput}>
         <label htmlFor="name">Name</label>
         <input id="name" ref={this.inputRef} />
-        <button onClick={this.loadGreetingForInput}>Load Greeting</button>
+        <button type="submit">Load Greeting</button>
         <div data-testid="greeting">{this.state.greeting}</div>
-      </div>
+      </form>
     )
   }
 }
