57ffdf8e45998e73c3429755e037b51ebe913ddc
Kent C. Dodds <kent@doddsfamily.us>
2018-09-07 09:43:21 -0600

    post recording

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -3,16 +3,14 @@ import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
 import React from 'react'
-import {render, wait} from 'react-testing-library'
+import {render, fireEvent, wait} from 'react-testing-library'
 import {loadGreeting as mockLoadGreeting} from '../api'
 import {GreetingLoader} from '../greeting-loader-01-mocking'
 
 jest.mock('../api', () => {
   return {
     loadGreeting: jest.fn(subject =>
-      Promise.resolve({
-        data: {greeting: `Hi ${subject}`},
-      }),
+      Promise.resolve({data: {greeting: `Hi ${subject}`}}),
     ),
   }
 })
@@ -22,8 +20,8 @@ test('loads greetings on click', async () => {
   const nameInput = getByLabelText(/name/i)
   const loadButton = getByText(/load/i)
   nameInput.value = 'Mary'
-  loadButton.click()
-  await wait(() => expect(getByTestId('greeting')).toHaveTextContent('Hi Mary'))
+  fireEvent.click(loadButton)
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+  await wait(() => expect(getByTestId('greeting')).toHaveTextContent(`Hi Mary`))
 })
