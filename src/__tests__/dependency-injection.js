43cf7c5e068e691754741ee40f42b477d8186e75
Kent C. Dodds <kent@doddsfamily.us>
2018-09-07 09:06:24 -0600

    update greeting-loader tests

--- src/__tests__/dependency-injection.js
+++ src/__tests__/dependency-injection.js
@@ -1,1 +1,25 @@
-test('tmp', () => {})
+// these should normally be in your jest setupTestFrameworkScriptFile
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import React from 'react'
+import {render, wait} from 'react-testing-library'
+import {GreetingLoader} from '../greeting-loader-01-dependency-injection'
+
+test('loads greetings on click', async () => {
+  const loadGreeting = jest.fn(subject =>
+    Promise.resolve({
+      data: {greeting: `Hi ${subject}`},
+    }),
+  )
+  const {getByLabelText, getByText, getByTestId} = render(
+    <GreetingLoader loadGreeting={loadGreeting} />,
+  )
+  const nameInput = getByLabelText(/name/i)
+  const loadButton = getByText(/load/i)
+  nameInput.value = 'Mary'
+  loadButton.click()
+  await wait(() => expect(getByTestId('greeting')).toHaveTextContent('Hi Mary'))
+  expect(loadGreeting).toHaveBeenCalledTimes(1)
+  expect(loadGreeting).toHaveBeenCalledWith('Mary')
+})
--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -1,1 +1,29 @@
-test('tmp', () => {})
+// these should normally be in your jest setupTestFrameworkScriptFile
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import React from 'react'
+import {render, wait} from 'react-testing-library'
+import {loadGreeting as mockLoadGreeting} from '../api'
+import {GreetingLoader} from '../greeting-loader-02-mocking'
+
+jest.mock('../api', () => {
+  return {
+    loadGreeting: jest.fn(subject =>
+      Promise.resolve({
+        data: {greeting: `Hi ${subject}`},
+      }),
+    ),
+  }
+})
+
+test('loads greetings on click', async () => {
+  const {getByLabelText, getByText, getByTestId} = render(<GreetingLoader />)
+  const nameInput = getByLabelText(/name/i)
+  const loadButton = getByText(/load/i)
+  nameInput.value = 'Mary'
+  loadButton.click()
+  await wait(() => expect(getByTestId('greeting')).toHaveTextContent('Hi Mary'))
+  expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
+  expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+})
