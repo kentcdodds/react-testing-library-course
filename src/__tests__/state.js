0ce3e859985cbdb1456a015bd3fd17884c29b17f
Kent C. Dodds <kent@doddsfamily.us>
2019-09-03 14:51:20 -0600

    string

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -5,6 +5,6 @@ import {FavoriteNumber} from '../favorite-number'
 test('entering an invalid value shows an error message', () => {
   const {getByLabelText, getByRole} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
-  fireEvent.change(input, {target: {value: 10}})
+  fireEvent.change(input, {target: {value: '10'}})
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
 })
