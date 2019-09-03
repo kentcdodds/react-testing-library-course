19a44492113d3103a61dae18f84bb1f1b21989e9
Kent C. Dodds <kent@doddsfamily.us>
2019-09-03 14:40:38 -0600

    improve and stuff

---
+++ src/__tests__/state-user-event.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import user from '@testing-library/user-event'
+import {render} from '@testing-library/react'
+import {FavoriteNumber} from '../favorite-number'
+
+test('entering an invalid value shows an error message', () => {
+  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const input = getByLabelText(/favorite number/i)
+  user.type(input, '10')
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+})
