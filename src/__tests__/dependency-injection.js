a068eb0892aceac3ebb1ca7e88027a5558e55429
Kent C. Dodds <kent@doddsfamily.us>
2019-09-03 21:52:15 -0600

    improve http-jest-mock

--- src/__tests__/dependency-injection.js
+++ src/__tests__/dependency-injection.js
@@ -3,9 +3,9 @@ import {render, fireEvent, waitForDomChange} from '@testing-library/react'
 import {GreetingLoader} from '../greeting-loader-02-dependency-injection'
 
 test('loads greetings on click', async () => {
-  const mockLoadGreeting = jest.fn(subject =>
-    Promise.resolve({data: {greeting: `Hi ${subject}`}}),
-  )
+  const mockLoadGreeting = jest.fn()
+  const testGreeting = 'TEST_GREETING'
+  mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
   const {getByLabelText, getByText} = render(
     <GreetingLoader loadGreeting={mockLoadGreeting} />,
   )
@@ -16,6 +16,6 @@ test('loads greetings on click', async () => {
   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
   await waitForDomChange(() =>
-    expect(getByLabelText('greeting')).toHaveTextContent(`Hi Mary`),
+    expect(getByLabelText('greeting')).toHaveTextContent(testGreeting),
   )
 })
--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -3,15 +3,11 @@ import {render, fireEvent, waitForDomChange} from '@testing-library/react'
 import {loadGreeting as mockLoadGreeting} from '../api'
 import {GreetingLoader} from '../greeting-loader-01-mocking'
 
-jest.mock('../api', () => {
-  return {
-    loadGreeting: jest.fn(subject =>
-      Promise.resolve({data: {greeting: `Hi ${subject}`}}),
-    ),
-  }
-})
+jest.mock('../api')
 
 test('loads greetings on click', async () => {
+  const testGreeting = 'TEST_GREETING'
+  mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
   const {getByLabelText, getByText} = render(<GreetingLoader />)
   const nameInput = getByLabelText(/name/i)
   const loadButton = getByText(/load/i)
@@ -20,6 +16,6 @@ test('loads greetings on click', async () => {
   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
   await waitForDomChange(() =>
-    expect(getByLabelText('greeting')).toHaveTextContent(`Hi Mary`),
+    expect(getByLabelText('greeting')).toHaveTextContent(testGreeting),
   )
 })
