a2df6ec7f9f70c044e5c16b1a3d61f1f7dac746b
Michael Urich <urich.michael@outlook.com>
2022-08-09 07:32:52 +0200

    chapter 5 done

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,14 +1,28 @@
 //import * as jestDOM from '@testing-library/jest-dom/dist/matchers'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  render,
+  queries,
+  screen,
+  getQueriesForElement,
+} from '@testing-library/react'
 const {FavoriteNumber} = require('favorite-number')
 
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
-  console.log(div.innerHTML)
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+  const {getByLabelText} = getQueriesForElement(div)
+  const input = getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
+})
+
+test('RTL: renders a number input with a label "Favorite Number"', () => {
+  //const div = document.createElement('div')
+  render(<FavoriteNumber />)
+  screen.getByLabelText(/favorite number/i)
+  //expect(div.querySelector('input')).toHaveAttribute('type', 'number')
   //  old
   //expect(div.querySelector('label').textContent).toBe('Favorite Number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  //expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
