bee2008d057fad37370fcddf34aaeffe8b053196
Michael Urich <urich.michael@outlook.com>
2022-08-10 20:32:41 +0200

    to check if an el is not there --> query* & .not.toBeInTheDocument

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,12 +1,12 @@
-//import * as jestDOM from '@testing-library/jest-dom/dist/matchers'
 import React from 'react'
 import {fireEvent, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 const {FavoriteNumber} = require('favorite-number')
 
 test('renders a number input with a label "Favorite Number"', () => {
+  // eslint-disable-next-line no-unused-vars
   const {getByLabelText, debug} = render(<FavoriteNumber />)
-  //debug()
+  // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
@@ -17,35 +17,29 @@ test('RTL: renders a number input NEW SCREEN APPROACH with a label "Favorite Num
   expect(screen.getByLabelText(/favorite number/i)).toBeInTheDocument()
 })
 
+//-----------------------------
+
+//-----------------------------
+
 test('OLD FIRE EVENT: entering an invalid value shows an error mesasge', () => {
-  const {getByLabelText, getByRole, debug} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i)
   fireEvent.change(input, {target: {value: '10'}})
-   debug()
   // eslint-disable-next-line testing-library/prefer-screen-queries
   expect(getByRole('alert')).toHaveTextContent(/The number is invalid/i)
 })
 
-test('NEW USER EVENT: entering an invalid value shows an error mesasge', async () => {
-  const { rerender } = render(<FavoriteNumber />)
+test('NEW USER EVENT: entering an invalid value shows an error mesasge', () => {
+  const {rerender} = render(<FavoriteNumber />)
   const input = screen.getByLabelText(/favorite number/i)
   userEvent.type(input, '10')
   //expect(screen.queryByRole(/alert/i)).not.toBeInTheDocument()
   expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  //screen.debug()
-  rerender(<FavoriteNumber max={10} />)
-  //screen.debug()
-})
 
-test('FROM KCD: entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
-    <FavoriteNumber />,
-  )
-  const input = getByLabelText(/favorite number/i)
-  userEvent.type(input, '10')
-  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug()
+  userEvent.clear(input)
   rerender(<FavoriteNumber max={10} />)
-  debug()
+  userEvent.type(input, '10')
+  // to check if a element is not rendered
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument()
 })
