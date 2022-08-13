d9a50698fc1f47efc43b4829d0ef9030554e96fb
Michael Urich <urich.michael@outlook.com>
2022-08-13 22:12:11 +0200

    added http mock

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -1,34 +1,28 @@
 import React from 'react'
-import {screen, render, fireEvent} from '@testing-library/react'
+// eslint-disable-next-line testing-library/prefer-wait-for
+import {screen, render, wait} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {GreetingLoader} from '../greeting-loader-01-mocking'
 import {loadGreeting as mockLoadGreeting} from '../api'
 
 const MY_GREETING = 'hello world'
-//jest.mock('../api')
-/* jest.mock('../api', () => {
-   return {
-      loadGreeting: MY_GREETING,
-   }
-}) */
+jest.mock('../api')
 
-test.skip('if the components greets corretly when request is submittes', () => {
-  //mockLoadGreeting.mockResolvedValueOnce(MY_GREETING)
+test('if the components greets corretly when request is submittes', async () => {
+  mockLoadGreeting.mockResolvedValueOnce({data: {greeting: MY_GREETING}})
 
   render(<GreetingLoader />)
   const name = screen.getByLabelText(/name/i)
   const submit = screen.getByRole('button', {name: /load greeting/i})
-  const greeting = screen.getByLabelText('greeting')
+  const greeting = screen.getByLabelText(/greeting/i)
 
-  expect(greeting).not.toHaveTextContent(greeting)
-  //userEvent.type(name, MY_GREETING)
+  expect(greeting).toHaveTextContent('')
+  userEvent.type(name, MY_GREETING)
   userEvent.click(submit)
+  // important to check that the mock is properly working
+  expect(mockLoadGreeting).toHaveBeenCalledWith(MY_GREETING)
+  expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
+  // if this is not wrapped, then there will be an ACT warning
+  // eslint-disable-next-line testing-library/prefer-wait-for
+  await wait(() => expect(greeting).toHaveTextContent(MY_GREETING))
 })
-
-test('loads greetings on click', () => {
-   const {getByLabelText, getByText} = render(<GreetingLoader />)
-   const nameInput = getByLabelText(/name/i)
-   const loadButton = getByText(/load/i)
-   nameInput.value = 'Mary'
-   fireEvent.click(loadButton)
- })
